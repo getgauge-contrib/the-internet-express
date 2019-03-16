daa136887079f038466257d9da9586ef34667e81
sriv <srikanth.ddit@gmail.com>
2019-03-16 12:14:59 +0530

    remove http logging

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 var multer  = require('multer')
 var fs = require('fs');
 
@@ -37,7 +36,6 @@ var isAuthenticated = (req, res, next) => {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
