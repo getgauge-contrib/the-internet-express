8bfd3289c7c9585bfc583386736b41eb343cfee0
Subham Singh <s.chauhan5505@gmail.com>
2019-09-09 12:18:11 +0530

    Added slow resouces page

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var dropdownRouter = require('./routes/dropdown');
 var basicAuthRouter = require('./routes/basic_auth');
 var dynamicLoadingRouter = require('./routes/dynamic_loading');
 var dragAndDropRouter = require('./routes/drag_and_drop');
+var slowResources = require('./routes/slow');
 var app = express();
 
 // view engine setup
@@ -35,6 +36,7 @@ app.use('/dropdown', dropdownRouter);
 app.use('/basic_auth', basicAuthRouter);
 app.use('/dynamic_loading', dynamicLoadingRouter);
 app.use('/drag_and_drop', dragAndDropRouter);
+app.use('/slow', slowResources);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
